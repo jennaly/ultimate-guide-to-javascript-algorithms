e73bcdafd44d7be382b3aa9501050c8976ca4717
jennaly <jenna@wicot.io>
2022-10-15 11:00:10 -0700

    reverseString solutions

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -3,12 +3,46 @@ Given a string of text, write an algorithm that returns the text received in a r
 E.g reverseString('algorithms') // should return 'smhtirogla'
 */
 
-
+// function reverseString(text) {
+//     return text.split('').reverse().join('')
+// }
 
 function reverseString(text) {
-    // Code goes here
+    return [...text].reverse().join('')
 }
 
+// function reverseString(text) {
+//     let result = "" 
+//     for (let i = text.length -1 ; i >= 0; i--) {
+//         result += text[i];
+//     }
 
+//     return result
+// }
+
+function reverseString(text) {
+    let result = "" 
+    for (let char of text) {
+        result = char + result;
+    }
+
+    return result
+}
+
+function reverseString(text) {
+    if (text === "") {
+        return ""
+    } else {
+        return reverseString(text.substring(1)) + text[0]
+    }
+}
+
+function reverseString(text) {
+    return text.split("").reduce((acc,char) => char + acc, '')
+}
+
+function reverseString(text) {
+    return [...text].reduce((acc,char) => char + acc, '')
+}
 
 module.exports = reverseString
